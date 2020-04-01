4a1e05cc0fcbe2e5dec3f931c49b836fabac1f3e
winter_silence <jormungand67@gmail.com>
2020-04-01 17:49:32 +0300

    Made folder Dialogs, css class appWrapperContent for grid

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import styles from './App.module.css';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
+import Dialogs from './Components/Dialogs/Dialogs';
 
 const App = () => {
   return (
     <div className={styles.appWrapper}>
-      <Header/>
-      <Navbar/>
-      <Profile/>
+      <Header />
+      <Navbar />
+      <div className={styles.appWrapperContent}>
+        {/* <Profile/> */}
+      </div>
+      <div className={styles.appWrapperContent}>
+        <Dialogs />
+      </div>
     </div>
   );
 }
