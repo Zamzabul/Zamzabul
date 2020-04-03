fb2a4fa380d45836918c1e4b9bf7f980d917bd55
winter_silence <jormungand67@gmail.com>
2020-04-03 15:11:02 +0300

    Textarea in Messages and button Send Message

--- src/Components/Dialogs/Messages/Messages.jsx
+++ src/Components/Dialogs/Messages/Messages.jsx
@@ -4,9 +4,22 @@ import Message from './Message/Message';
 
 const Messages = (props) => {
     let messagesElements = props.messagesElements.map(e => <Message text={e.text}/>)
+    let newMessageElement = React.createRef();
+    let sendMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
     return (
         <div className={styles.messages}>
-            {messagesElements}
+            <div>
+                {messagesElements}
+            </div>
+            <div>
+                <textarea ref={newMessageElement}></textarea>
+            </div>
+            <div>
+                <button onClick={sendMessage}>Send message</button>
+            </div>
         </div>
     )
 }
